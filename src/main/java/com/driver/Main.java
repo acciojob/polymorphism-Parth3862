2a4415608e58a9f5fdba378ee4f781f4701d3e1b
Parth <parthudgirkar@gmail.com>
2024-02-11 16:55:26 +0530

    Added a file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String args[]) {
+        Product p = new Product();
+        int ans = p.Product(2,5);
+        int ans1 = p.Product(2,5,6);
+        double ans2 = p.Product(2.0,7.0);
+        System.out.println(ans+" "+ans1+" "+ans2);
+    }
+    public static class Product {
+        public int Product(int x,int y){
+            return x*y;
+        }
+        public int Product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double Product(double x,double y){
+            return x*y;
+        }
+    }
 }
