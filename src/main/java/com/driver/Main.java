5dc7efa6cbba269877a98cc50c31d33183026821
Parth <parthudgirkar@gmail.com>
2024-02-11 17:18:55 +0530

    updated code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String args[]) {
         Product p = new Product();
-        int ans = p.Product(2,5);
-        int ans1 = p.Product(2,5,6);
-        double ans2 = p.Product(2.0,7.0);
+        int ans = p.product(2,5);
+        int ans1 = p.product(2,5,6);
+        double ans2 = p.product(2.0,7.0);
         System.out.println(ans+" "+ans1+" "+ans2);
     }
     public static class Product {
